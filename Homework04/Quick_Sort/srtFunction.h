ec35823e4a69e0581155b1e08190c9d41924749b
Nikunj Rathod <nikunjrathod3011@gmail.com>
2020-02-07 01:41:25 -0400

    Quick Sort

--- Homework04/Quick_Sort/srtFunction.h
+++ Homework04/Quick_Sort/srtFunction.h
@@ -3,5 +3,5 @@
 #include<vector>
 
 std::vector<int> qSort(std::vector<int>& v);
-void qsortWork(int low, int high, std::vector<int>& v);
-int partition(int low, int high, std::vector<int>& v);
+void qsortWork(int l, int h, std::vector<int>& v);
+int partition(int l, int h, std::vector<int>& v);
--- Homework04/Quick_Sort/test.cpp
+++ Homework04/Quick_Sort/test.cpp
@@ -1,7 +1,46 @@
+/**
+* @file <test.cpp>
+*
+* @author  <Nikunj Rathod>
+* @version <1.0>
+*
+* change log
+* name      date
+*
+*
+* (file, auther, version, and change log are depricated with modern source code management system)
+*
+* @section Academic Integrity
+*  I certify that this work is solely my own and complies with
+*  NBCC Academic Integrity Policy (policy 1111)
+*
+*
+* @section DESCRIPTION
+*  < Date in format >
+*
+*
+* @section LICENSE
+*  <Public>
+*
+*  Copyright 2020
+*  Permission to use, copy, modify, and/or distribute this software for
+*  any purpose with or without fee is hereby granted, provided that the
+*  above copyright notice and this permission notice appear in all copies.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+*  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+*  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+*  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+*  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+*  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+*  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*
+*/
+
 #include "gtest/gtest.h"
 #include <vector>
 #include "srtFunction.h"
-//bubble sort 1
+//Quick sort 1
 TEST(TestCaseName, TestQsortCallFunc01) {
 	std::vector<int> v = { 113,22,414,122,256,922,1 };
 	std::vector<int> vExpected = { 1,22,113,122,256,414,922 };
@@ -13,7 +52,7 @@ TEST(TestCaseName, TestQsortCallFunc01) {
 	ASSERT_EQ(vResult.size(), vExpected.size());
 	ASSERT_TRUE(isSame);
 }
-//bubble sort 2
+//Quick sort 2
 TEST(TestCaseName, TestQsortCallFunc02) {
 	std::vector<int> v = { 1,1,1,1,1,53,22,2,21,3,1 };
 	std::vector<int> vExpected = { 1,1,1,1,1,1,2,3,21,22,53 };
@@ -26,10 +65,10 @@ TEST(TestCaseName, TestQsortCallFunc02) {
 	ASSERT_TRUE(isSame);
 }
 
-//selection sort random 10k numbers
+//Quick sort random 1m numbers
 TEST(TestCaseName, TestQSortCallFunc04) {
 	srand((size_t)0);
-	int SIZE = 100000 / 3;
+	int SIZE = 1000000;
 	std::vector<int> v;
 	v.reserve(SIZE);
 
